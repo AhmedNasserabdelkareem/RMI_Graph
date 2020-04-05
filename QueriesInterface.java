3b77e3fe7813fe34769d0629942eb6463d982097
ziyadelbanna <ziyadelbanna1997@yahoo.com>
2020-04-05 20:09:35 +0300

    all files complete

--- QueriesInterface.java
+++ QueriesInterface.java
@@ -13,7 +13,6 @@ public interface QueriesInterface extends Remote{
 	   void addEdge(int firstID,int secondID) throws RemoteException;
 	   void deleteEdge(int firstID,int secondID) throws RemoteException;
 	   int shortestPath(int firstID,int secondID) throws RemoteException;
-		 ArrayList<Integer> BFSShortestPath(int source, int dest)  throws RemoteException;
 	   String getGraph() throws RemoteException;
 
 }
